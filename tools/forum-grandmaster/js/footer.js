a5f2f76274e691aef2c852fee2af7a5a33137bab
admin <admin@example.com>
2020-04-29 09:26:49 +0000

    content update

--- tools/forum-grandmaster/js/footer.js
+++ tools/forum-grandmaster/js/footer.js
@@ -12,14 +12,38 @@ const app = new Vue({
     data: {
         content: 'Hello HTML!'
     },
-    created: function () {
+    created () {
         console.log(this.content);
         console.log(typeof window.forumGrandmaster);
         console.log(window.forumGrandmaster);
     },
     methods: {
-        handleDark: function () {
+        handleDark () {
             this.$q.dark.set(!this.$q.dark.isActive);
+        },
+        handleSettingsDefault () {
+            this.$q.dialog({
+                title: '论坛大师',
+                message: '已经恢复默认设置',
+            }).onOk(() => {
+                // console.log('OK')
+            }).onCancel(() => {
+                // console.log('Cancel')
+            }).onDismiss(() => {
+                // console.log('I am triggered on both OK and Cancel')
+            });
+        },
+        handleSettingsSave () {
+            this.$q.dialog({
+                title: '论坛大师',
+                message: '所有设置已经保存',
+            }).onOk(() => {
+                // console.log('OK')
+            }).onCancel(() => {
+                // console.log('Cancel')
+            }).onDismiss(() => {
+                // console.log('I am triggered on both OK and Cancel')
+            });
         }
     }
 })
