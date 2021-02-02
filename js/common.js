321e59e7045d651aa8c744733c61bd0ed6675c25
admin <admin@example.com>
2021-02-02 14:04:37 +0000

    content update

--- js/common.js
+++ js/common.js
@@ -19,24 +19,17 @@
  *                    '.:::::'                    ':'````..
  */
 
-window.addEventListener('contextmenu', function (event) {
+window.addEventListener('contextmenu', (event) => {
     event.preventDefault();
 }, false);
-window.addEventListener('keydown', function (event) {
-    if (event.which === 123) {
+window.addEventListener('keydown', (event) => {
+    if (event.key === 'F12') {
         event.preventDefault();
     }
 }, false);
 
-if (window.location.hash) {
-    history.replaceState('', '', window.location.href.split('#')[0]);
-}
-if (window.location.search) {
-    history.replaceState('', '', window.location.href.split('?')[0]);
-}
-
-const DATE = new Date();
-const DATETIME = DATE.getFullYear() * 10000 + DATE.getMonth() * 100 + 100 + DATE.getDate();
+!!window.location.hash && history.replaceState('', '', window.location.href.split('#')[0]);
+!!window.location.search && history.replaceState('', '', window.location.href.split('?')[0]);
 
 let device = '';
 if (/(Android|iPhone|Mobile|OppoBrowser)/.test(window.navigator.userAgent)) {
@@ -44,3 +37,8 @@ if (/(Android|iPhone|Mobile|OppoBrowser)/.test(window.navigator.userAgent)) {
 } else {
     device = 'd';
 }
+
+const device = /(Android|iPhone|Mobile|OppoBrowser)/.test(window.navigator.userAgent) ? 'm' : 'd';
+
+const DATE = new Date();
+const DATETIME = DATE.getFullYear() * 10000 + DATE.getMonth() * 100 + 100 + DATE.getDate();
